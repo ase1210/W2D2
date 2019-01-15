6e20aea6406d99fbc278f8447c3ac4fc9c86447e
DrewE <andrew.engelstein@gmail.com>
2019-01-15 12:21:19 -0800

    Add #move_dirs to sliding piece subclasses, Add module skeletons for SlidingPiece and SteppingPiece

--- chess/piece.rb
+++ chess/piece.rb
@@ -1,9 +1,33 @@
 require "singleton"
 
-class Piece
-  def initialize
-    
+module SlidingPiece
+
+  def moves(current_pos, directions)
+    moves = []
+
+
+    moves
   end
+  
+end
+
+module SteppingPiece
+  
+  def moves(current_pos)
+  
+  end
+
+end
+
+
+class Piece
+
+  def initialize
+    @color = :white
+    @symbol = :P
+    @current_pos = [0, 1]
+  end
+  
 end
 
 class King < Piece
@@ -12,6 +36,10 @@ end
 
 class Queen < Piece
 
+  def move_dirs
+    [:up, :right, :left, :down, :diag_up_right, :diag_up_left, :diag_down_right, :diag_down_left]
+  end
+
 end
 
 class Knight < Piece
@@ -19,14 +47,27 @@ class Knight < Piece
 end
 
 class Bishop < Piece
+  include SlidingPiece
 
+  def move_dirs
+    [:diag_up_right, :diag_up_left, :diag_down_right, :diag_down_left]
+  end
+  
 end
 
 class Rook < Piece
+  
+  def move_dirs
+    [:up, :right, :left, :down]
+  end
 
 end
 
 class Pawn < Piece
+  
+  # def move_dirs
+  #   [:up]
+  # end
 
 end
 
