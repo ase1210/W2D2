26f9e57687b679c29164b0240ef719931393d16c
DrewE <andrew.engelstein@gmail.com>
2019-01-15 13:53:20 -0800

    Add #moves for SlidingPiece module

--- chess/piece.rb
+++ chess/piece.rb
@@ -4,7 +4,59 @@ module SlidingPiece
 
   def moves(current_pos, directions)
     moves = []
-
+    directions.each do |dir|
+      case dir 
+      when :up 
+        new_pos = current_pos 
+        until new_pos[0] == 0
+          new_pos[0] -= 1
+          moves << new_pos 
+        end
+      when :right 
+        new_pos = current_pos 
+        until new_pos[1] == 7
+          new_pos[1] += 1
+          moves << new_pos 
+        end
+      when :left
+        new_pos = current_pos 
+        until new_pos[1] == 0
+          new_pos[1] -= 1
+          moves << new_pos 
+        end 
+      when :down 
+        new_pos = current_pos 
+        until new_pos[0] == 7
+          new_pos[0] += 1
+          moves << new_pos 
+        end
+      when :diag_up_left
+        new_pos = current_pos 
+        until new_pos[0] == 0 || new_pos[1] == 0
+          new_pos[0] -= 1
+          new_pos[1] -= 1
+          moves << new_pos 
+        end
+      when :diag_up_right
+        until new_pos[0] == 0 || new_pos[1] == 7
+          new_pos[0] -= 1
+          new_pos[1] += 1
+          moves << new_pos 
+        end
+      when :diag_down_right
+        until new_pos[0] == 7 || new_pos[1] == 7
+          new_pos[0] += 1
+          new_pos[1] += 1
+          moves << new_pos 
+        end
+      when :diag_down_left
+        until new_pos[0] == 7 || new_pos[1] == 0
+          new_pos[0] += 1
+          new_pos[1] -= 1
+          moves << new_pos 
+        end
+      end 
+    end
 
     moves
   end
@@ -17,7 +69,7 @@ module SteppingPiece
   
   end
 
-end
+end 
 
 
 class Piece
